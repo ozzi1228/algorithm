cfe4e8ed00f825a00f24524cd8c85588a8d13835
leeozzi <lstory1997l@gmail.com>
2025-12-30 15:30:40 +0900

    BOJ10172 개

---
+++ CLASS1/BOJ10172.java
@@ -0,0 +1,13 @@
+package CLASS1;
+
+public class BOJ10172 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+        System.out.println(sb);
+    }
+}
