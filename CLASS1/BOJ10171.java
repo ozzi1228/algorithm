1ece9ef6943a125558e4124d373a84b45d2bd42e
leeozzi <lstory1997l@gmail.com>
2025-12-30 15:27:39 +0900

    BOJ10171 고양이

---
+++ CLASS1/BOJ10171.java
@@ -0,0 +1,12 @@
+package CLASS1;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+        System.out.println(sb);
+    }
+}
