0709e8587160a63c33660066bf2881783b725a41
leeozzi <lstory1997l@gmail.com>
2026-01-12 12:34:33 +0900

    A+B - 3

---
+++ CLASS1/BOJ10950.java
@@ -0,0 +1,23 @@
+package CLASS1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A + B).append("\n");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
