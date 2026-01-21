961a52987c158c6f8cde79959bb2032a9eb07ddb
이언지 <lstory1997l@gmail.com>
2026-01-21 10:15:13 +0900

    BOJ11654 아스키코드 * char 끼리 연산하면 아스키코드 연산이 된다는 점을 활용

---
+++ CLASS1/BOJ11654.java
@@ -0,0 +1,13 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.nextLine().charAt(0);
+        System.out.println('0' + input - '0');
+    }
+
+}
