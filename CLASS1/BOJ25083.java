e1b765e3695311c388bb1b61b353ae127191b1cc
이언지 <lstory1997l@gmail.com>
2026-01-21 10:17:28 +0900

    BOJ25083 새싹

---
+++ CLASS1/BOJ25083.java
@@ -0,0 +1,16 @@
+package CLASS1;
+
+public class BOJ25083 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+        System.out.println(sb);
+    }
+
+}
