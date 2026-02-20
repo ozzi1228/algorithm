9d265648cd50cec9c90f3197715f228d0912b542
leeozzi <lstory1997l@gmail.com>
2026-02-21 05:50:08 +0900

    BOJ2798 블랙잭 * 역순정렬 안 해줘서 그런가 했는데 별 차이 없음. 심지어 int 배열이었다가 Integer 배열로 바꾸면서 메모리는 더 커짐

--- CLASS2/BOJ2797_recursive.java
+++ CLASS2/BOJ2797_recursive.java
@@ -3,12 +3,14 @@ package CLASS2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class BOJ2797_recursive {
 
   static int N, M;
-  static int[] arr;
+  static Integer[] arr;
   static boolean[] visited;
   static int max = Integer.MIN_VALUE;
 
@@ -19,11 +21,13 @@ public class BOJ2797_recursive {
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
     st = new StringTokenizer(br.readLine());
-    arr = new int[N];
+    arr = new Integer[N];
     visited = new boolean[N];
     for (int i = 0; i < N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    Arrays.sort(arr, Collections.reverseOrder());
 
     getSum(0, 0);
     System.out.println(max);
